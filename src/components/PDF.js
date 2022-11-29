e053018be6c37463987e484220b3c63c3250a540
Joey Santia <joey.santia@gmail.com>
2022-11-29 15:33:00 -0500

    First draft of PDF component

--- src/components/PDF.js
+++ src/components/PDF.js
@@ -7,16 +7,49 @@ export default class PDF extends React.Component {
     }
 
     generatePDF() {
-        let doc = new jsPDF()
-        let contactResponses = this.props.responses[0].responses
+        let [
+            contacts,
+            education, 
+            employment,
+            skills
+        ] = this.props.responses
 
+        let doc = new jsPDF()
         doc.text(this.props.firstName + ' ' + this.props.lastName, 10, 10)
-        doc.text(contactResponses[3].value, 10, 20)
-        doc.text(contactResponses[4].value, 10, 30)
-        doc.text(contactResponses[5].value, 10, 40)
-        doc.text(contactResponses[6].value, 10, 50)
-        doc.text(contactResponses[7].value, 10, 60)
-        
+
+        let x = 10
+        let y = 20
+
+        for (let i = 2; i < contacts.responses.length; i++) {
+            doc.text(contacts.responses[i].value, x, y)
+            y += 10
+        }
+
+        doc.text(education.title, x, y)
+        y += 10
+
+        for (const response of education.responses) {
+            doc.text(response.value, x, y)
+            y += 10
+        }
+
+        doc.text(employment.title, x, y)
+        y += 10
+
+        for (const response of employment.responses) {
+            doc.text(response.value, x, y)
+            y += 10
+        }
+
+        doc.text(skills.title, x, y)
+        y += 10
+
+        for (const response of skills.responses) {
+            doc.text(response.value, x, y)
+            y += 10
+        }
+
+        //let contactResponses = this.props.responses[0].responses
         doc.save(`${this.props.firstName}${this.props.lastName}Resume.pdf`)
         return doc 
     }
